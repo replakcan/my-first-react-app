79192a94d3234c201352d927a2b7831f1f3fe00e
replakcan <alper.makcan@gmail.com>
2025-06-19 14:33:12 +0300

    Creates router with createBrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import RenderName from "./components/RenderName.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import Profile from "./components/Profile.jsx";
+import Spinach from "./components/Spinach.jsx";
+import Popeye from "./components/Popeye.jsx";
+import DefaultProfile from "./components/DefaultProfile.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "profile",
+    element: <Profile />,
+    children: [
+      { index: true, element: <DefaultProfile /> },
+      { path: "spinach", element: <Spinach /> },
+      { path: "popeye", element: <Popeye /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RenderName />
+    <RouterProvider router={router} />
   </StrictMode>
 );
