191dbaf589def17764b9660b9fa67e4fc5f28c57
replakcan <alper.makcan@gmail.com>
2025-06-19 14:02:51 +0300

    Updates main

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import ClassInput from "./class-components/ClassInput.jsx";
-import FunctionalInput from "./class-components/FunctionalInput.jsx";
+import RenderName from "./components/RenderName.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClassInput name={"Class Input"} />
-    <FunctionalInput name={"Functional Input"} />
+    <RenderName />
   </StrictMode>
 );
